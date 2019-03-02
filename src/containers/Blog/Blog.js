c0ae37e2a2c032c37f112a9bea6477a8c347e447
Ryan Moore <rhcmoore@gmail.com>
2019-03-02 14:20:45 -0800

    Implement FullPost methods

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -28,12 +29,18 @@ class Blog extends Component {
             })
     }
 
+    // for expanding clicked post information in FullPost
+    postSelected = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
             return <Post 
                 key={post.id}
                 title={post.title}
                 author={post.author}
+                clicked={()=>this.postSelected(post.id)}
                 />
         });
 
@@ -43,7 +50,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
