8a20cfc922c03ea68ca59d22ce946e83ac5907e4
Ryan Moore <rhcmoore@gmail.com>
2019-03-02 15:03:36 -0800

    Implement fake delete request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -8,7 +8,6 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-
     componentDidUpdate() {
         if (this.props.id) {
             // prevent infitite looping 
@@ -19,6 +18,13 @@ class FullPost extends Component {
                     })
             }
         }
+    }
+
+    deletePostHandler = () => {
+        axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+            .then(response => {
+                console.log(response);
+            })
     }
 
     render() {
@@ -32,7 +38,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
                     </div>
                 </div>
 
